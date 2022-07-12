0e4d0b10ae3b84caee1af672e8d59d8265958965
kds <kdskoudous98@gmail.com>
2022-07-12 19:26:01 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+
+let taskBtn = document.getElementsByClassName('task-btn');
+let submitBtn = document.querySelector(".submit-btn");
+// console.log(taskBtn);
+let taskNumber = document.querySelector('.task-number');
+let ratingDiv = document.querySelector('.rating-div');
+let thankDiv = document.querySelector('.thank-div')
+
+
+let number = 0;
+
+for (let i=0; i < taskBtn.length; i++) {
+    taskBtn[i].addEventListener('click', () => {
+        number = taskBtn[i].id;
+        console.log(number);
+        taskNumber.textContent = "You selected " + number + " out of 5";
+    })
+}
+
+submitBtn.addEventListener('click', () => {
+    ratingDiv.classList.add("close");
+    thankDiv.classList.add("active");
+})
+
+
+
+
+
+
+
+
+
+
